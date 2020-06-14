85b961c3c3c621ec73f1ea2d94fdbce430d0402f
Tristan Buckner <tristanbuckner@gmail.com>
2020-06-13 22:42:18 -0700

    fix

--- src/main/scala/OfferService.scala
+++ src/main/scala/OfferService.scala
@@ -9,11 +9,18 @@ import akka.stream.ActorMaterializer
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor}
 
+/**
+ * Since I haven't used Akka http before, I deferred to the organization in the example project.
+ * In real life I think you'd want to somehow factor out the routes for easier testing,
+ * but I would need to read more example projects to see how to best do that.
+ */
 object OfferService extends App with OfferJsonSupport {
     implicit val system: ActorSystem = ActorSystem("example-interview")
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
+    // In reality you'd probably want a clock in the requesting user's local time,
+    // or use ZonedDateTime on the experiments if the expiration matters that much.
     implicit val clock: Clock = Clock.systemUTC()
 
     val filePath = "/experiments.json"
@@ -38,18 +45,17 @@ object OfferService extends App with OfferJsonSupport {
             (get & path("offers") & parameters("phone", "score".as[Double])) { (phone, score) =>
                 validate(phoneMatcher.test(phone), "Invalid Phone number") {
                     validate(validateScore(score), "Score must be between 0.0D and 1.0D") {
-                        complete(offerLogic.getOffers(score).map(_.toOfferExternal))
+                        complete(offerLogic.getOffers(score).map(_.toOfferExternal)) //Strip out minScore per the readme
                     }
                 }
             }
 
         val port = 9000
-        val binding = Http().bindAndHandle(
-            routes,
-            "127.0.0.1",
-            port
-        )
+
+        val binding = Http().bindAndHandle(routes, "127.0.0.1", port)
+
         println(s"Server started on port $port")
+
         Await.result(system.whenTerminated, Duration.Inf)
     } catch {
         case ValidationException(errors) => println(s"Errors found in file at ${filePath}:")
